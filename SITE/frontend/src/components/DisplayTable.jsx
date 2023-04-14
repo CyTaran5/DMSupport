faf2715763873af0b4c896a1319447b6f03150d1
JohnNelson2 <johnnelsonmusic2@gmail.com>
2023-04-13 22:34:12 -0600

    Basic table is now displayed

--- SITE/frontend/src/components/DisplayTable.jsx
+++ SITE/frontend/src/components/DisplayTable.jsx
@@ -10,54 +10,44 @@ import {
     Th,
     Td,
     TableCaption,
+    TableContainer,
   } from "@chakra-ui/react"
 
 
 const DisplayTable = ({jsonTableData}) => {
   console.log("Display table has json data:", jsonTableData);
-  
+
+  if (!jsonTableData || jsonTableData.length === 0) {
+    return (
+      <div>
+        <h1>No data available</h1>
+      </div>
+    );
+  }
+  const headers = jsonTableData.length > 0 ? Object.keys(jsonTableData[0]) : [];
+
   return (
-    <div>
-      <h1>Data reciever component</h1>
-      <p>Recieved JSON DATA:</p>
-      <pre>{JSON.stringify(jsonTableData, null, 2)}</pre>
-    </div>
+    <TableContainer>
+      <Table varient = 'simple'>
+        <Thead>
+          <Tr>
+            {headers.map((header, index) => (
+            <Th key={index}>{header}</Th>
+            ))}
+          </Tr>
+        </Thead>
 
-//   <Table variant="simple">
-//   <TableCaption>Imperial to metric conversion factors</TableCaption>
-//   <Thead>
-//     <Tr>
-//       <Th>To convert</Th>
-//       <Th>into</Th>
-//       <Th isNumeric>multiply by</Th>
-//     </Tr>
-//   </Thead>
-//   <Tbody>
-//     <Tr>
-//       <Td>inches</Td>
-//       <Td>millimetres (mm)</Td>
-//       <Td isNumeric>25.4</Td>
-//     </Tr>
-//     <Tr>
-//       <Td>feet</Td>
-//       <Td>centimetres (cm)</Td>
-//       <Td isNumeric>30.48</Td>
-//     </Tr>
-//     <Tr>
-//       <Td>yards</Td>
-//       <Td>metres (m)</Td>
-//       <Td isNumeric>0.91444</Td>
-//     </Tr>
-//   </Tbody>
-//   <Tfoot>
-//     <Tr>
-//       <Th>To convert</Th>
-//       <Th>into</Th>
-//       <Th isNumeric>multiply by</Th>
-//     </Tr>
-//   </Tfoot>
-// </Table>
-
+        <Tbody>          
+          {jsonTableData.map((data, index) => (
+            <Tr key={index}>
+              {headers.map((header, index) => (
+                <Td key={index}>{data[header]}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   )
 };
 
